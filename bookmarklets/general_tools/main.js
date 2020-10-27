849b8264ed2c007e0d35f6c5a673ea7d78cec930
notTGY <microcraft47@gmail.com>
2020-10-27 17:55:27 +0300

    bookmarklets started

---
+++ bookmarklets/general_tools/main.js
@@ -0,0 +1,39 @@
+(function () {
+
+  /*Check if we had already loaded extension*/
+
+  /*
+  if (TGY_bookmarklet_JStool_v1 == undefined) {
+    TGY_bookmarklet_JStool_v1 = true;
+  } else {
+    return 1;
+  }
+  */
+
+  /*Add canvas if we can*/
+  /*
+  if (mainCanvas_TGY_bookmarklet_JStool_v1 == undefined) {
+    mainCanvas_TGY_bookmarklet_JStool_v1 = document.createElement('canvas');
+  } else {
+    return 1;
+  }
+  */
+
+
+  mainCanvas_TGY_bookmarklet_JStool_v1 = document.createElement('canvas');//FIXME
+
+  /* local variable mainCanvas for easier access */
+  let mainCanvas = mainCanvas_TGY_bookmarklet_JStool_v1;
+  mainCanvas.style.position='fixed';
+  mainCanvas.style.backgroundColor='#55A8';
+  mainCanvas.style.top = 0;
+  mainCanvas.style.left = 0;
+  mainCanvas.width = 100;
+  mainCanvas.height = 100;
+  document.body.appendChild(mainCanvas);
+
+
+
+
+
+})();
