02f02211fd1788b5d2ecb114f8aa7682e374e616
notTGY <microcraft47@gmail.com>
2020-11-01 18:27:14 +0300

    a lot of work, refactoring, some experiments

--- bookmarklets/general_tools/main.js
+++ bookmarklets/general_tools/main.js
@@ -1,104 +1,202 @@
-/* Global variables:
-
-mainCanvas_TGY_JStool_v1 - mainCanvas
-
-*/
-
 (function () {
+  /* magic numbers */
 
-  /*Check if we had already loaded extension*/
+  const MAINCANVAS_ID = 'mainCanvas';
+  const MAINCANVASDOWN_ID = 'mainCanvas_down';
+  const CLOSE_BUTTON_CANVAS_ID = 'close_button_canvas';
+  const SELECTOR_CANVAS_ID = 'selector_canvas';
 
-  /*
-  if (TGY_bookmarklet_JStool_v1 == undefined) {
-    TGY_bookmarklet_JStool_v1 = true;
-  } else {
-    return 1;
-  }
-  */
-
-  /*Add canvas if we can*/
-
-  /*
-  if (mainCanvas_TGY_JStool_v1 == undefined) {
-    mainCanvas_TGY_JStool_v1 = document.createElement('canvas');
-  } else {
-    return 1;
-  }
-  */
+  const SIZE_OF_SMALL_CANVASES = 100;
+  const CLOSING_CANVAS_CROSS_PERCENTAGE = 0.8;
+  const ARROW_BUTTONS_COLOR = '#999F';
+  const BLOCK_BUTTONS_COLOR = '#FFFF';
 
 
-  mainCanvas_TGY_JStool_v1 = document.createElement('canvas');//FIXME
+  /* initialization */
+  const mainCanvas = document.createElement('canvas');
+  let is_selector_launched = 0;
+  let is_selector_created = 0;
+  const close_button_canvas = document.createElement('canvas');
 
   /* Add id to accept stylesheet */
-  mainCanvas_TGY_JStool_v1.id = 'mainCanvas_TGY_JStool_v1';
+  mainCanvas.id = MAINCANVAS_ID;
+  mainCanvas.width = SIZE_OF_SMALL_CANVASES;
+  mainCanvas.height = SIZE_OF_SMALL_CANVASES;
 
-  mainCanvas_TGY_JStool_v1.width = 100;
-  mainCanvas_TGY_JStool_v1.height = 100;
+  close_button_canvas.id = CLOSE_BUTTON_CANVAS_ID;
+  close_button_canvas.hidden = true;
+
+  document.body.appendChild(mainCanvas);
+  document.body.appendChild(close_button_canvas);
+
+  const mainCtx = mainCanvas.getContext('2d');
 
 
-  document.body.appendChild(mainCanvas_TGY_JStool_v1);
+  /*dependency functions*/
 
-  let mainCtx = mainCanvas_TGY_JStool_v1.getContext('2d');
+  /* draws light-gray triangle on the given ctx, at given coords, s is size of each side
+  x and y are coords of top left corner */
+  function drawTriangleDown(ctx,x,y,s) {
+    ctx.fillStyle = ARROW_BUTTONS_COLOR;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.lineTo(x + s,y);
+    ctx.lineTo(x + s/2, y + s*Math.sqrt(3)/2);
+    ctx.fill();
+  }
+  /* draws light-gray triangle on the given ctx, at given coords, s is size of each side
+  x and y are coords of top left corner */
+  function drawTriangleUp(ctx,x,y,s) {
+    ctx.fillStyle = ARROW_BUTTONS_COLOR;
+    ctx.beginPath();
+    ctx.moveTo(x + s/2, y);
+    ctx.lineTo(x,y + s*Math.sqrt(3)/2);
+    ctx.lineTo(x + s,y + s*Math.sqrt(3)/2);
+    ctx.fill();
+  }
+  function drawBlocks(ctx,x,y,s) {
+    ctx.fillStyle = BLOCK_BUTTONS_COLOR;
+    ctx.fillRect(x,y,s/5,s/5);
+    ctx.fillRect(x+2*s/5,y,s/5,s/5);
+    ctx.fillRect(x+4*s/5,y,s/5,s/5);
+    ctx.fillRect(x,y+2*s/5,s/5,s/5);
+    ctx.fillRect(x+2*s/5,y+2*s/5,s/5,s/5);
+    ctx.fillRect(x+4*s/5,y+2*s/5,s/5,s/5);
+    ctx.fillRect(x,y+4*s/5,s/5,s/5);
+    ctx.fillRect(x+2*s/5,y+4*s/5,s/5,s/5);
+    ctx.fillRect(x+4*s/5,y+4*s/5,s/5,s/5);
+  }
+
+  function makeUp(mainCtx,w,h) {
+    mainCanvas.id = MAINCANVAS_ID;
+    mainCtx.clearRect(0, 0, w, h);
+    drawTriangleDown(mainCtx,w/2+w/16,h/2+h/16,Math.min(w/2-w/8,h/2-h/8));
+    drawBlocks(mainCtx,w/16,h/16,Math.min(w/2-w/8,h/2-h/8));
+  }
+
+  function makeDown(mainCtx,w,h) {
+      mainCanvas.id = MAINCANVASDOWN_ID;
+      mainCtx.clearRect(0, 0, w, h);
+      drawTriangleUp(mainCtx,w/2+w/16,h/16,Math.min(w/2-w/8,h/2-h/8));
+      drawBlocks(mainCtx,w/16,h/2+h/16,Math.min(w/2-w/8,h/2-h/8));
+  }
+
+  function launchSelector() {
+    if (!is_selector_launched) {
+      console.log('selector launched');
+
+      if (!is_selector_created) {
+        selector_canvas = document.createElement('canvas');
+        selector_canvas.id = SELECTOR_CANVAS_ID;
+        document.body.appendChild(selector_canvas);
+        is_selector_created = 1;
+      }
+
+      selector_canvas.offsetLeft = mainCanvas.width + mainCanvas.offsetLeft;
+
+      selector_canvas.width = window.innerWidth - selector_canvas.offsetLeft - close_button_canvas.width;
+      selector_canvas.height = window.innerHeight;
+
+      selector_canvas.hidden = false;
+      close_button_canvas.hidden = false;
+      is_selector_launched = 1;
+    } else {
+      console.log('selector is already launced');
+    }
+  }
+  function closeSelector() {
+    if (is_selector_launched) {
+      console.log('selector closed');
+      selector_canvas.hidden = true;
+      close_button_canvas.hidden = true;
+      is_selector_launched = 0;
+    } else {
+      console.log('couldn\'t find selector');
+    }
+
+  }
+
+  function initClosingCanvas() {
+      let ctx = close_button_canvas.getContext('2d');
+      let k = CLOSING_CANVAS_CROSS_PERCENTAGE;
+      let w = close_button_canvas.width;
+      let h = close_button_canvas.height;
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+      ctx.moveTo(w/2-k*w/2,h/2-k*h/2);
+      ctx.lineTo(w/2+k*w/2,h/2+k*h/2);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(w/2-k*w/2,h/2+k*h/2);
+      ctx.lineTo(w/2+k*w/2,h/2-k*h/2);
+      ctx.stroke();
+  }
+
+
+  /* init pictires */
+  makeUp(mainCtx,mainCanvas.width,mainCanvas.height);
+
+  initClosingCanvas();
+
 
   /* Click handler */
   document.addEventListener('click', e=>{
-    let mainCtx = mainCanvas_TGY_JStool_v1.getContext('2d');
-
-    let x = mainCanvas_TGY_JStool_v1.offsetLeft - document.body.offsetLeft;
-    let y = mainCanvas_TGY_JStool_v1.offsetTop - document.body.offsetTop;
-    let w = mainCanvas_TGY_JStool_v1.width;
-    let h = mainCanvas_TGY_JStool_v1.height;
-    let id = mainCanvas_TGY_JStool_v1.id;
-    let X = e.clientX;
-    let Y = e.clientY;
-
-    if (id == 'mainCanvas_TGY_JStool_v1') {
-
+    const mainCtx = mainCanvas.getContext('2d');
+    let x = mainCanvas.offsetLeft - document.body.offsetLeft;
+    let y = mainCanvas.offsetTop - document.body.offsetTop;
+    let w = mainCanvas.width;
+    let h = mainCanvas.height;
+    const id = mainCanvas.id;
+    const X = e.clientX;
+    const Y = e.clientY;
+    if (id == MAINCANVAS_ID) {
       /* check if we need to move this thing down */
       if (X <= x+w && Y <= y+h) {
         if (X > x+w/2 && Y > y+h/2) {
-          mainCanvas_TGY_JStool_v1.id = 'mainCanvas_TGY_JStool_v1_down';
-          drawTriangleUp(mainCtx,x+w/2,y+h/2,Math.min(w/2,h/2));
+          makeDown(mainCtx,w,h);
           return 0;
         }
       }
-
+      if (X <= x+w/2 && Y <= y+h/2) {
+        if (X > x && Y > y) {
+          launchSelector();
+          return 0;
+        }
+      }
       /* other buttons */
-
-    } else if (id == 'mainCanvas_TGY_JStool_v1_down'){
-
+    } else if (id == MAINCANVASDOWN_ID){
       /* check if we need to move this thing up */
       if (X <= x+w && Y <= y+h/2) {
         if (X > x+w/2 && Y > y) {
-          mainCanvas_TGY_JStool_v1.id = 'mainCanvas_TGY_JStool_v1';
-          drawTriangleDown(mainCtx,x+w/2,y+h/2,Math.min(w/2,h/2));
+          makeUp(mainCtx,w,h);
           return 0;
         }
       }
-
+      if (X <= x+w/2 && Y <= y+h) {
+        if (X > x && Y > y+h/2) {
+          launchSelector();
+          return 0;
+        }
+      }
       /* other buttons */
-
     }
-
-
+    x = close_button_canvas.offsetLeft - document.body.offsetLeft;
+    y = close_button_canvas.offsetTop - document.body.offsetTop;
+    w = close_button_canvas.width;
+    h = close_button_canvas.height;
+    if (!close_button_canvas.hidden) {
+      if(X>x && X<x+w) {
+        if (Y>y && Y<y+h) {
+          closeSelector();
+          return 0;
+        }
+      }
+    }
   });
 
+  /* keydown handler */
+  document.addEventListener('keydown', e=>{
+    ;
+  });
 
-
-    /* keydown handler */
-    document.addEventListener('keydown', e=>{
-
-
-    });
-
-    /* draws light-gray triangle on the given ctx, at given coords, s is size of each side
-    x and y are coords of top left corner */
-    function drawTriangleDown(ctx,x,y,s) {
-      ;
-    }
-    /* draws light-gray triangle on the given ctx, at given coords, s is size of each side
-    x and y are coords of top left corner */
-    function drawTriangleUp(ctx,x,y,s) {
-      ;
-    }
 })();
