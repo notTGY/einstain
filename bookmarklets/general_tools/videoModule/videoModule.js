c534eb30a8750edae15136b133268c8c063b9515
notTGY <microcraft47@gmail.com>
2020-11-16 12:19:25 +0300

    a lot of mess with the progress bar

--- bookmarklets/general_tools/videoModule/videoModule.js
+++ bookmarklets/general_tools/videoModule/videoModule.js
@@ -140,8 +140,8 @@
     if (bar) {
       let str = 'linear-gradient(to right, #FFF 0%, #FF0 ';
       let prog = vidElem.currentTime / vidElem.duration;
-      str += Math.floor(100*prog/2) + '%, #FFF ';
-      str += Math.floor(100*prog) + '%)';
+      str += Math.floor(100*prog) + '%, #FFF0 ';
+      str += Math.floor(100*1) + '%)';
       console.log(str);
       bar.style.background = str;
     }
