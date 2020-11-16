50db563ea2547f291b44de7f91f12ba00c8661dd
notTGY <microcraft47@gmail.com>
2020-11-16 18:09:53 +0300

    sometimes booleans are hard

--- bookmarklets/general_tools/videoModule/videoModule.js
+++ bookmarklets/general_tools/videoModule/videoModule.js
@@ -288,7 +288,7 @@
     if (startTime.hours != undefined && startTime.minutes != undefined) {
       let isPlaying = (vidElem.currentTime > 0 && !vidElem.paused && !vidElem.ended && vidElem.readyState > 2);
       let logic = (startTime.hours*60+startTime.minutes > hours*60+minutes);
-      if (logic) {
+      if (!logic) {
         if (!isPlaying) {
           countdownOverlay.style.opacity = 0;
           hookPlayButton(); 
