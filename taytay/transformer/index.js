797933814b23b3caeea2fbd65d8233c4ef41ce0b
notTGY <microcraft47@gmail.com>
2024-03-18 17:41:05 +0300

    tuning for local development

--- taytay/transformer/index.js
+++ taytay/transformer/index.js
@@ -47,17 +47,81 @@ const countLetters = (text) => {
   }
   return {countByLetter, letterCount, clean}
 }
-const saveTrainingData = (songsData) => {
+
+const SEPARATORS = [' ', '\n', ',', '"', '(', ')', '-', '\'', '?', '$']
+const countWords = (text) => {
+  const countByWord = {}
+  let uniqueWords = 0
+  let wordCount = 0
+  let word = ''
+  let splitByWords = []
+  const cleanText = text + ' '
+  for (let i = 0; i < cleanText.length; i++) {
+    const letter = cleanText[i]
+    if (SEPARATORS.includes(letter)) {
+      if (word !== '') {
+        splitByWords.push(word)
+        if (countByWord[word]) {
+          countByWord[word]++
+          wordCount++
+        } else {
+          countByWord[word] = 1
+          uniqueWords++
+          wordCount++
+        }
+        word = ''
+      }
+      if (countByWord[letter]) {
+        countByWord[letter]++
+        wordCount++
+      } else {
+        countByWord[letter] = 1
+        uniqueWords++
+        wordCount++
+      }
+    } else {
+      word += letter
+    }
+  }
+  return { uniqueWords, wordCount, countByWord, splitByWords }
+}
+const saveTrainingData = ({songsData, wordFreq}) => {
   const clean = songsData.map(s => s.clean)
   const text = clean.join('~')
-  download('data.txt', text)
+  download('songs.txt', text)
+
+  const words = wordFreq.map(w => w.word).join('~')
+  download('words.txt', words)
 }
 const doSong = (song) => {
   const letterData = countLetters(song.Lyrics)
+  const wordData = countWords(letterData.clean)
   return {
+    ...wordData,
     ...letterData,
     song,
   }
+}
+const wordFreqAnal = (songsData) => {
+  const wordFreq = {}
+  for (const song of songsData) {
+    for (const word in song.countByWord) {
+      if (!wordFreq[word]) {
+        wordFreq[word] = 0
+      }
+      wordFreq[word] += song.countByWord[word]
+    }
+  }
+  const dic = []
+  for (const word in wordFreq) {
+    if (!SEPARATORS.includes(word) && wordFreq[word] > 1 && word.length > 1) {
+      dic.push({word, count: wordFreq[word]})
+    }
+  }
+  dic.sort((data1, data2) => {
+    return data2.count - data1.count
+  })
+  return dic
 }
 const doStuff = (songs) => {
   const filteredSongs = songs.filter(song => {
@@ -65,7 +129,9 @@ const doStuff = (songs) => {
     return true
   })
   const songsData = filteredSongs.map(song => doSong(song))
-  //saveTrainingData(songsData)
+  const wordFreq = wordFreqAnal(songsData)
+  console.log({wordFreq})
+  saveTrainingData({songsData, wordFreq})
 
   const song = songsData[0]
   const str = '~'+song.clean.substring(0, 7)
